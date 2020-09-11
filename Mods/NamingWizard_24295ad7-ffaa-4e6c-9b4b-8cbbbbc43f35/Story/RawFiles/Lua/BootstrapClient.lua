a3449dc69c1c7dbbbc3bb2eba022156f39d87c97
LaughingLeader <laughingleader@gmail.com>
2020-09-10 19:01:41 -0500

    Rename the Wand of Renaming weapon type

---
+++ Mods/NamingWizard_24295ad7-ffaa-4e6c-9b4b-8cbbbbc43f35/Story/RawFiles/Lua/BootstrapClient.lua
@@ -0,0 +1,22 @@
+---@param item EsvItem
+---@param tooltip TooltipData
+local function OnItemTooltip(item, tooltip)
+	if item ~= nil and item:HasTag("LLNWIZ_RenamingWand") then
+		local element = tooltip:GetElement("ArmorSlotType")
+		if element == nil then
+			element = {
+				Type = "ArmorSlotType",
+				Label = ""
+			}
+			tooltip:AppendElement(element)
+		end
+		element.Label = Ext.GetTranslatedStringFromKey("LLNWIZ_RenamingWand")
+	end
+end
+
+Ext.RegisterListener("SessionLoaded", function()
+	Game.Tooltip.RegisterListener("Item", nil, OnItemTooltip)
+	---@type TranslatedString
+	--local ts = Mods.LeaderLib.Classes.TranslatedString
+	--UI.RegisterItemTooltipTag("LLNWIZ_RenamingWand", ts:Create("", ""), ts:Create("", ""))
+end)
